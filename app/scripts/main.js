81b5ee944ac6a48d67d780c17e603c8183005ba6
Mihail Paley <undeadlol1@mail.ru>
2015-10-18 23:33:05 +0500

    Apply fix for big viewing video on big screens
    On big screens video preview were shrinked down for design purposes.
    After user clicks the video it goes fullscreen now.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,9 @@
+'use strict';
 $(document).ready(function() {
     //enable smoothScroll
     $('a').smoothScroll();
 
+    //enable prettyEmbed for better youtube preview
     $('#embed-video').prettyEmbed({
         videoID: 'Cbti19KM3wk',
         useFitVids: true,
@@ -10,7 +12,12 @@ $(document).ready(function() {
         playerInfo: false
     });
 
+    //on big screens video preview is shrinked. After user clicks play button video goes full fullscreen
     $('.pretty-embed, .pretty-embed img').click(function() {
-        $(this).css('max-height', 'none');
+        $('#video .col-md-12').css('position', 'static');
+        $.smoothScroll({
+            scrollElement: $('body'),
+            scrollTarget: '#video'
+        });
     });
 });
